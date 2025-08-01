c920aa0b7f68435ac3b8229d8d618e9b02508652
smileynm <smileynm@naver.com>
2025-08-01 20:39:54 +0900

    Add: some control logics

--- controller/viewcontroller.cpp
+++ controller/viewcontroller.cpp
@@ -1,5 +1,17 @@
 #include "viewcontroller.h"
 
 ViewController::ViewController() {
+    qDebug() << "ViewController initiated";
+}
 
+void ViewController::AdminViewHandler() {
+    m_adminInfoForm = new AdminInfoForm(nullptr);
+    m_adminChatForm = new AdminChatForm(nullptr);
+    m_adminProductForm = new AdminProductForm(nullptr);
+    m_adminOrderListForm = new AdminOrderListForm(nullptr);
+}
+
+void ViewController::NormalViewHandler() {
+    m_normalChatForm = new NormalChatForm(nullptr);
+    m_normalOrderForm = new NormalOrderForm(nullptr);
 }
--- controller/viewcontroller.h
+++ controller/viewcontroller.h
@@ -1,9 +1,32 @@
 #ifndef VIEWCONTROLLER_H
 #define VIEWCONTROLLER_H
+#include "admininfoform.h"
+#include "adminchatform.h"
+#include "adminorderlistform.h"
+#include "adminproductform.h"
+#include "normalchatform.h"
+#include "normalorderform.h"
+
+class AdminInfoForm;
+class AdminChatForm;
+class AdminOrderListForm;
+class AdminProductForm;
+
+class NormalChatForm;
+class NormalOrderForm;
 
 class ViewController {
 public:
     ViewController();
+    void AdminViewHandler();
+    void NormalViewHandler();
+private:
+    AdminInfoForm *m_adminInfoForm;
+    AdminChatForm *m_adminChatForm;
+    AdminOrderListForm *m_adminOrderListForm;
+    AdminProductForm *m_adminProductForm;
+    NormalChatForm *m_normalChatForm;
+    NormalOrderForm *m_normalOrderForm;
 };
 
 #endif // VIEWCONTROLLER_H
