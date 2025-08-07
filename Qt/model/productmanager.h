4988ae6ccb0ccc7c5c5174d2c5bdbe6bafc3ee8a
hahahowon <howon.kim3667@gmail.com>
2025-08-07 17:52:19 +0900

    라즈베리파이, Qt 연결 완료

--- Qt/model/productmanager.h
+++ Qt/model/productmanager.h
@@ -8,21 +8,30 @@
 #include <QMap>
 #include "product.h"
 #include "orderedproduct.h"
-
+#include "qrproduct.h"
 class ProductManager :public QObject {
     Q_OBJECT
-
+signals:
+    void updateQrMap();
 public:
     static ProductManager& getInstance();
+
+    bool removeProduct(const QString& productID);
     bool registerProduct(Product* product, const QString& productID);
     bool registerOrderedProducts(OrderedProduct* product, const QString& productID);
+    bool registerQrProducts(QrProduct* product, const QString& productID);
+
     Product* findProductByCategory(const QString& productCategory);
     Product* findProductByName(const QString& productName);
-    bool removeProduct(const QString& productID);
+    Product* findProductByID(const QString& productId);
+
     const QMap<QString, Product*>& getProductMap() const;
     const QMap<QString, OrderedProduct*>& getOrderedProductMap() const;
+    const QMap<QString, QrProduct*>& getQrProductMap() const;
 
-    QMap<QString, OrderedProduct*> orderedProducts;
+    void receiveQrData(const QByteArray& Qrdata);
+
+
 private:
     // 싱글턴 구현을 위한 생성자/소멸자의 private 접근제한
     ProductManager();
@@ -32,7 +41,10 @@ private:
     ProductManager(const ProductManager&) = delete;
     ProductManager& operator=(const ProductManager&) = delete;
 
+
     QMap<QString, Product*> productsByID;
+    QMap<QString, OrderedProduct*> orderedProducts;
+    QMap<QString, QrProduct*> qrProduct;
 };
 
 #endif // PRODUCTMANAGER_H
